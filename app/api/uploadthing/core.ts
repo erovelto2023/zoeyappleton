17323fa7bb0a975ffeb8ddd2cbc508e5c21fd11c
erovelto2023 <123208905+erovelto2023@users.noreply.github.com>
2026-01-05 13:35:22 -0500

    update

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -8,8 +8,15 @@ const handleAuth = () => {
 
 export const ourFileRouter = {
     imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
-        .middleware(() => handleAuth())
-        .onUploadComplete(() => { }),
+        .middleware(async () => {
+            const auth = handleAuth();
+            return auth;
+        })
+        .onUploadComplete(async ({ metadata, file }) => {
+            console.log("Upload complete for userId:", metadata.userId);
+            console.log("file url", file.url);
+            return { uploadedBy: metadata.userId, url: file.url };
+        }),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
